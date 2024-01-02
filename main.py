763073ad967b909c93ac10ed311cca88800395db
Xenvar2307 <lukaszkniemczyk@gmail.com>
2024-01-02 22:23:20 +0100

    dynamic scales

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ pygame.init()
 # display
 screen_width = 1600
 screen_height = 900
+side_padding = 50
+ground_level = screen_height - side_padding
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("MiSK - project")
+
+# font
+scale_font = pygame.font.SysFont("Times New Roman", 20)
 
 # frames
 fps = 60
@@ -19,8 +24,110 @@ white = (255, 255, 255)
 meters_to_pixel_ratio = 100
 
 
+def get_scale_step(input: int) -> int:  # 0,1,2 -> 1,2,5; 3,4,5 -> 10,20,50
+    multiply = 10 ** int(input / 3)
+    match input % 3:
+        case 0:
+            return multiply
+        case 1:
+            return 2 * multiply
+        case 2:
+            return 5 * multiply
+
+
+def meters_to_text(meters: int):
+    if meters < 1000:
+        return f"{meters}m"
+    else:
+        return f"{meters/1000}km"
+
+
 def reset_screen():
     screen.fill(black)
+
+
+def draw_scale():
+    # base lines
+    pygame.draw.line(
+        screen,
+        white,
+        (side_padding, ground_level),
+        (side_padding, side_padding),
+        width=4,
+    )
+
+    pygame.draw.line(
+        screen,
+        white,
+        (side_padding, ground_level),
+        (screen_width - side_padding, ground_level),
+        width=4,
+    )
+
+    # get scale step
+    temp = 0
+    step = get_scale_step(temp)
+    while step * meters_to_pixel_ratio < 50:
+        temp += 1
+        step = get_scale_step(temp)
+
+    # scale x
+    meters = 0
+    start = side_padding
+    count_lines = 1
+    lines_to_draw = int(
+        (screen_width - 2 * side_padding) / (meters_to_pixel_ratio * step)
+    )
+    text_step = int(lines_to_draw / 5)
+    first = True
+    while count_lines <= lines_to_draw:
+        meters += step
+        start += step * meters_to_pixel_ratio
+        pygame.draw.line(
+            screen,
+            white,
+            (start, ground_level),
+            (start, ground_level + int(0.25 * side_padding)),
+            width=4,
+        )
+
+        if count_lines % text_step == 0 or first:
+            text_image = scale_font.render(meters_to_text(meters), True, white)
+            text_image_rect = text_image.get_rect()
+            text_image_rect.center = (start, ground_level + int(0.5 * side_padding))
+            screen.blit(text_image, text_image_rect)
+
+        count_lines += 1
+        first = False
+
+    # scale y
+    meters = 0
+    start = ground_level
+    count_lines = 1
+    lines_to_draw = int(
+        (screen_height - 2 * side_padding) / (meters_to_pixel_ratio * step)
+    )
+    text_step = int(lines_to_draw / 5)
+    first = True
+    while count_lines <= lines_to_draw:
+        meters += step
+        start -= step * meters_to_pixel_ratio
+        pygame.draw.line(
+            screen,
+            white,
+            (side_padding, start),
+            (side_padding - int(0.25 * side_padding), start),
+            width=4,
+        )
+
+        if count_lines % text_step == 0 or first:
+            text_image = scale_font.render(meters_to_text(meters), True, white)
+            text_image_rect = text_image.get_rect()
+            text_image_rect.center = (side_padding - int(0.5 * side_padding), start)
+            screen.blit(text_image, text_image_rect)
+
+        count_lines += 1
+        first = False
 
 
 class Trebuchet:
@@ -34,7 +141,12 @@ class main_module:
         run = True
         Next_module = Module_names.Exit_app
 
+        global meters_to_pixel_ratio
+        meters_to_pixel_ratio = 0.1
+
         while run:
+            draw_scale()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
